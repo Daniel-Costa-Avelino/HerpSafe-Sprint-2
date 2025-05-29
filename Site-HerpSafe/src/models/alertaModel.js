97ea333185dc0b25a5ed90889ee4917682844561
guigtoledo <guilherme.toledo@sptech.school>
2025-05-29 19:28:52 -0300

    fix: ajuste na model do alerta com a mudança da tabela (de alerta para captura)

--- Site-HerpSafe/src/models/alertaModel.js
+++ Site-HerpSafe/src/models/alertaModel.js
@@ -15,6 +15,21 @@ function buscarAlertas(fkEmpresa) {
     return database.executar(instrucaoSql);
 }
 
+function buscarAlertas(fkEmpresa) {
+
+    var instrucaoSql = `
+    SELECT * FROM captura
+    JOIN sensor ON fkSensor = idSensor
+    JOIN recinto on fkRecinto = idRecinto
+    JOIN prateleira ON fkPrateleira = idPrateleira
+    JOIN empresa ON fkEmpresa_prateleira = idEmpresa
+    WHERE idEmpresa = ${fkEmpresa};
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     buscarAlertas
 }
