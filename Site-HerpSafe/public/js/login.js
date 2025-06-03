f2c5922fdbf67e3545bcab662ebc191b77041ce1
EzequielC-Dev <ezequiel34324@gmail.com>
2025-06-02 23:19:50 -0300

    refactor: integração com o toastify e tratamento de erros

--- Site-HerpSafe/public/js/login.js
+++ Site-HerpSafe/public/js/login.js
@@ -1,42 +1,80 @@
 function login() {
   var email = document.getElementById("input_emailLogin").value;
   var senha = document.getElementById("input_senhaLogin").value;
-  var codigo = document.getElementById("input_codigoLogin").value;
-}
 
-if (email == "" || senha == "" || codigo == "") {
-  alert(`Preencha todos os campos para continuar.`);
-}
+  if (email == "" || senha == "") {
+    Toastify({
+      text: "Erro: Por favor, preencha todos os campos",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "#0B371F",
+      },
+    }).showToast();
+  }
 
-var corpo = {
-  emailServer: email,
-  senhaServer: senha,
-  codigoServer: codigo,
-};
+  var corpo = {
+    emailServer: email,
+    senhaServer: senha,
+  };
 
-fetch("/usuarios/autenticar", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(corpo),
-})
-  .then(function (resposta) {
-    if (resposta.ok) {
-      return resposta.json();
-    } else {
-      return resposta.text().then((msg) => {
-        throw new Error(msg);
-      });
-    }
+  fetch("/usuarios/autenticar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(corpo),
   })
-  .then(function (dados) {
-    alert("Login realizado com sucesso!");
-    console.log("Usuário logado:", dados);
+    .then(function (resposta) {
+      if (resposta.ok) {
+        return resposta.json();
+      } else {
+        return resposta.text().then((msg) => {
+          throw new Error(msg);
+        });
+      }
+    })
+    .then(function (dados) {
+      Toastify({
+        text: "Login Realizado com Sucesso!",
+        duration: 250,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "#0B371F",
+        },
+      }).showToast();
 
-    window.location.href = "dashboard.html";
-  })
-  .catch(function (erro) {
-    console.error("Erro ao tentar login:", erro.message);
-    document.getElementById("p_mensagem").innerText = "Erro: " + erro.message;
-  });
+      sessionStorage.EMAIL_USUARIO = dados.email;
+      sessionStorage.NOME_USUARIO = dados.nome;
+      sessionStorage.ID_EMPRESA = dados.fkEmpresa;
+
+      window.location.href = "./dashboard/dashboard-visao-geral.html";
+    })
+    .catch(function (erro) {
+      Toastify({
+        text: "Erro: Não foi possível realizar a requisição",
+        duration: 250,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "#0B371F",
+        },
+      }).showToast();
+
+      console.error("Erro ao tentar login:", erro.message);
+    });
+}
